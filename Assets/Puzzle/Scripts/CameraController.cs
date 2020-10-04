4f3537c7ceca5a415da91d607c8482a5c82f65b1
hiro-49 <kazuki97101@gmail.com>
2020-10-05 08:22:41 +0900

    カメラの簡単な設定とステージ生成
    カメラの初期位置をゆかりさんに
    カメラがステージからはみ出さないように
    ステージを配置
    ゆかりさんを配置

---
+++ Assets/Puzzle/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject yukarisan;
+    public float stageHeight;
+    public float stageWidth;
+    float cameraHeight = 1080f;
+    float cameraWidth = 1920f;
+
+    float up_max;
+    float down_max;
+    float right_max;
+    float left_max;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 position = transform.position;
+        if (position.y > up_max) position.y = up_max;
+        if (position.y < down_max) position.y = down_max;
+        if (position.x > right_max) position.x = right_max;
+        if (position.x < left_max) position.x = left_max;
+        position.z = 0f;
+        transform.position = position;
+    }
+
+    //カメラ設定初期化
+    public void Init()
+    {
+        up_max = stageHeight / 2 - cameraHeight / 2;
+        down_max = cameraHeight / 2 - stageHeight / 2;
+        right_max = stageWidth / 2 - cameraWidth / 2;
+        left_max = cameraWidth / 2 - stageWidth / 2;
+        transform.position = yukarisan.transform.position;
+    }
+}
--- Assets/Puzzle/Scripts/PuzzleManager.cs
+++ Assets/Puzzle/Scripts/PuzzleManager.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
+    //プレハブ
+    public GameObject DummmyStage;
+    public GameObject Yukarisan;
+
+    public GameObject camera;
+    CameraController cameraController;
+    GameObject stage;
+    StageController stageController;
+    GameObject yukarisan;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ステージ配置
+        stage = Instantiate(DummmyStage, new Vector3(0f, 0f, 10f), Quaternion.identity);
+        stageController = stage.GetComponent<StageController>();
+        //ゆかりさん配置
+        Transform start = stage.transform.Find("Start");
+        yukarisan = Instantiate(Yukarisan, start.position, Quaternion.identity);
+        //カメラ設定
+        cameraController = camera.GetComponent<CameraController>();
+        cameraController.yukarisan = yukarisan;
+        cameraController.stageHeight = stageController.stageHeight;
+        cameraController.stageWidth = stageController.stageWidth;
+        cameraController.Init();    //初期化
     }
 
     // Update is called once per frame
@@ -16,3 +37,4 @@ public class PuzzleManager : MonoBehaviour
         
     }
 }
+    
