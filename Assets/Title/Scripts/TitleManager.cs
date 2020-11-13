f9b504e2378ff405162f3b12e64924c6e44ecc1e
hiro-49 <kazuki97101@gmail.com>
2020-11-13 13:24:16 +0900

    タイトル画面からゲームが始まらない不具合の修正

--- Assets/Title/Scripts/TitleManager.cs
+++ Assets/Title/Scripts/TitleManager.cs
@@ -5,52 +5,69 @@ using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
-    public GameObject canvas;
+    //ボタンクリックでなくスペースキーで遷移するように
+    //今後、続きから機能などの実装の時に戻す可能性あり
 
-    GameObject gameSelect;
-    GameObject stageSelect;
+    //public GameObject canvas;
 
-    private void Start()
+    //GameObject gameSelect;
+    //GameObject stageSelect;
+
+    //private void Start()
+    //{
+    //    gameSelect = canvas.transform.Find("GameSelect").gameObject;
+    //    //ステージ選択画面は初期状態で隠しておく
+    //    stageSelect = canvas.transform.Find("StageSelect").gameObject;
+    //        //未クリアステージは選択不可に
+    //    for(int i = 0; i < GameManager.Instance.StageMax; i++)
+    //    {
+    //        string buttonName = "StageNum_" + i.ToString();
+    //        stageSelect.transform.Find(buttonName).gameObject.SetActive(GameManager.Instance.data.clearedStage[i]);
+    //    }
+    //    stageSelect.SetActive(false);
+    //    //NewGameを選択状態にしておく
+    //    gameSelect.transform.Find("NewGame").gameObject.GetComponent<Button>().Select();
+    //}
+
+    private void Update()
     {
-        gameSelect = canvas.transform.Find("GameSelect").gameObject;
-        //ステージ選択画面は初期状態で隠しておく
-        stageSelect = canvas.transform.Find("StageSelect").gameObject;
-            //未クリアステージは選択不可に
-        //for(int i = 0; i < GameManager.Instance.StageMax; i++)
-        //{
-        //    string buttonName = "StageNum_" + i.ToString();
-        //    stageSelect.transform.Find(buttonName).gameObject.SetActive(GameManager.Instance.data.clearedStage[i]);
-        //}
-        stageSelect.SetActive(false);
-        //NewGameを選択状態にしておく
-        gameSelect.transform.Find("NewGame").gameObject.GetComponent<Button>().Select();
+        CheckSpaceKey();
     }
 
-    public void OnClickNewGame()
+    void CheckSpaceKey()
     {
-        //GameManager.Instance.nextStageKey = "1-1";
-        GameManager.Instance.LoadPuzzleScene("1-1");
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GameManager.Instance.LoadPuzzleScene("1-1");
+        }
     }
 
-    public void OnClickLoadGame()
-    {
-        //ゲーム選択画面を隠してステージ選択画面に切り替える
-        gameSelect.SetActive(false);
-        stageSelect.SetActive(true);
-        stageSelect.transform.Find("StageNum_0").gameObject.GetComponent<Button>().Select();
-    }
 
-    public void OnClickStageNum(string stageKey)
-    {
-        //GameManager.Instance.StageSelect(stageKey);
-        GameManager.Instance.LoadPuzzleScene(stageKey);
-    }
+    //public void OnClickNewGame()
+    //{
+    //    //GameManager.Instance.nextStageKey = "1-1";
+    //    GameManager.Instance.LoadPuzzleScene("1-1");
+    //}
 
-    public void OnClickBack()
-    {
-        //ゲーム選択画面に切り替える
-        gameSelect.SetActive(true);
-        stageSelect.SetActive(false);
-        gameSelect.transform.Find("NewGame").gameObject.GetComponent<Button>().Select();
-    }
+    //public void OnClickLoadGame()
+    //{
+    //    //ゲーム選択画面を隠してステージ選択画面に切り替える
+    //    gameSelect.SetActive(false);
+    //    stageSelect.SetActive(true);
+    //    stageSelect.transform.Find("StageNum_0").gameObject.GetComponent<Button>().Select();
+    //}
+
+    //public void OnClickStageNum(string stageKey)
+    //{
+    //    //GameManager.Instance.StageSelect(stageKey);
+    //    GameManager.Instance.LoadPuzzleScene(stageKey);
+    //}
+
+    //public void OnClickBack()
+    //{
+    //    //ゲーム選択画面に切り替える
+    //    gameSelect.SetActive(true);
+    //    stageSelect.SetActive(false);
+    //    gameSelect.transform.Find("NewGame").gameObject.GetComponent<Button>().Select();
+    //}
 }
